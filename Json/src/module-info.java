b005ef548ace733d747f07ee43accf29756b879c
Maros Maly <marosmaly97@gmail.com>
2022-05-21 22:53:56 +0200

    save 21-5

--- Json/src/module-info.java
+++
@@ -1,3 +0,0 @@
-module jsonJavaEE {
-	requires java.json;
-}
--- ServletChat/src/main/java/balicek/Chat.java
+++ ServletChat/src/main/java/balicek/Chat.java
@@ -19,133 +19,90 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
-
 @WebServlet("/Chat")
 public class Chat extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-	private ObjectMapper om = new ObjectMapper();
 	private static int ids = 0;
        
- 
     public Chat() {
-        super();
-        
+        super();        
     }
-
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		checkUsers(request, response);
-		Map <Integer,String> users = (Map<Integer, String>) request.getServletContext().getAttribute("users");
-		List <String> messages = (List<String>) request.getServletContext().getAttribute("messages");
-		int skip = (int) request.getSession().getAttribute("skip");
-		
-		//ODOSLEM NOVU SPRAVU UZIVATELOM
-		ObjectNode on = om.createObjectNode();
-		JsonNode lasdjfoisdj = om.valueToTree(messages.subList(skip, messages.size()));
-		on.put("messages", lasdjfoisdj);
-		lasdjfoisdj = om.valueToTree(users.values());
-		on.put("users", lasdjfoisdj);
-
-		//ODPOVED SERVERA
-		response.getWriter().append(on.toString());
-		
-		
+    
+    protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getSession().setAttribute("skip", ((List<String>) request.getServletContext().getAttribute("messages")).size());
 	}
 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {		
+		Map <Integer,Date> time = (Map<Integer, Date>) request.getServletContext().getAttribute("time");
+		Map <Integer,String> users = (Map<Integer, String>) request.getServletContext().getAttribute("users");		
+		Date actualTime = new Date();	
+		for (Entry<Integer, Date> itr : time.entrySet()) {
+			if (actualTime.getTime() - itr.getValue().getTime() > 60*1000) {
+				users.remove(itr.getKey());	
+			}
+		}
+		List <String> messages = (List<String>) request.getServletContext().getAttribute("messages");
+		int skip = (int) request.getSession().getAttribute("skip");
+		JsonNode sprava;
+		ObjectMapper obj_map = new ObjectMapper();
+		ObjectNode slovnik = obj_map.createObjectNode();
+		sprava = obj_map.valueToTree(messages.subList(skip, messages.size()));
+		slovnik.put("messages", sprava);
+		sprava = obj_map.valueToTree(users.values());
+		slovnik.put("users", sprava);		
+		response.getWriter().append(slovnik.toString());
+	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		BufferedReader brr = request.getReader();
-		JsonNode json = om.readTree(brr);
-		int aktId;
-		
-		//INICIALIZUJEM STORAGE
+		ObjectMapper obj_map = new ObjectMapper();
+		JsonNode sprava = obj_map.readTree(brr);
+		int user_id;
 		if (request.getServletContext().getAttribute("users") == null) {
 			request.getServletContext().setAttribute("users", new HashMap<Integer, String>());
 			request.getServletContext().setAttribute("messages", new ArrayList<String>());
 			request.getServletContext().setAttribute("time", new HashMap<Integer, Date>());
-		}
-		
+		}		
 		List <String> messages = (List<String>) request.getServletContext().getAttribute("messages");
 		Map <Integer,String> users = (Map<Integer, String>) request.getServletContext().getAttribute("users");
 		Map <Integer,Date> time = (Map<Integer, Date>) request.getServletContext().getAttribute("time");
-		
-		//ZISTIM, CI MA UZIVATEL ID 
 		if (request.getSession().getAttribute("id") == null) {
-			aktId = ids++;
-			request.getSession().setAttribute("id", aktId);
+			user_id = ids;
+			ids += 1;
+			request.getSession().setAttribute("id", user_id);
 			request.getSession().setAttribute("skip", 0);
-			request.getSession().setAttribute("name", json.get("name").asText());
-			
-			time.put(aktId, new Date());
-			
-			//pridam do globalenho zoznamu mien
-			users.put(aktId, json.get("name").asText());
-			return;
-		}
-		
-		checkUsers(request, response);
-		aktId = (int) request.getSession().getAttribute("id");
-		
-		if (!users.containsKey(aktId)) {
-			users.put(aktId, (String) request.getSession().getAttribute("name")); 
-		}
-		
-		time.put(aktId, new Date());
-		int skip = (int) request.getSession().getAttribute("skip");
-		String userName = users.get(aktId);
-		
-		System.out.println(users);
-		//CITAT POST A PRIDAM POSLEDNU SPRAVU DO SPRAV NA GLOBAL ULOZISKU
-		String message = json.get("message").asText();
-		messages.add(userName + ": " + message);
-		
-		//ODOSLEM NOVU SPRAVU UZIVATELOM
-		ObjectNode on = om.createObjectNode();
-		
-		
-		JsonNode lasdjfoisdj = om.valueToTree(messages.subList(skip, messages.size()));
-		on.put("messages", lasdjfoisdj);
-		lasdjfoisdj = om.valueToTree(users.values());
-		on.put("users", lasdjfoisdj);
-		
-		//ODPOVED SERVERA
-		response.getWriter().append(on.toString());
-		
-		
-		
-		/*
-		//GLOBALNE ULOZISKO INFORMACII
-		request.getServletContext().setAttribute("chat", "Maros");
-		request.getServletContext().getAttribute("name");
-		
-		//SESSION MA INFO LEN O PRIRADENOM USEROVI
-		request.getSession().setAttribute("chat", "Maros");
-		request.getSession().getAttribute("chat");*/
-		
-		
+			request.getSession().setAttribute("name", sprava.get("name").asText());			
+			time.put(user_id, new Date());
+			users.put(user_id, sprava.get("name").asText());
+		} else {		
+			Date actualTime = new Date();		
+			for (Entry<Integer, Date> itr : time.entrySet()) {
+				if (actualTime.getTime() - itr.getValue().getTime() > 60*1000) {
+					users.remove(itr.getKey());	
+				}
+			}		
+			user_id = (int) request.getSession().getAttribute("id");			
+			if (!users.containsKey(user_id)) {
+				users.put(user_id, (String) request.getSession().getAttribute("name")); 
+			}			
+			time.put(user_id, new Date());
+			int skip = (int) request.getSession().getAttribute("skip");
+			String userName = users.get(user_id);
+			String message = sprava.get("message").asText();
+			messages.add(userName + ": " + message);			
+			ObjectNode slovnik = obj_map.createObjectNode();			
+			JsonNode new_sprava; 
+			new_sprava = obj_map.valueToTree(messages.subList(skip, messages.size()));
+			slovnik.put("messages", new_sprava);
+			new_sprava = obj_map.valueToTree(users.values());
+			slovnik.put("users", new_sprava);
+			response.getWriter().append(slovnik.toString());
+		}	
 	}
 	
 	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		Map <Integer,String> users = (Map<Integer, String>) request.getServletContext().getAttribute("users");
-		int aktId = (int) request.getSession().getAttribute("id");
-		users.remove(aktId);
+		int user_id = (int) request.getSession().getAttribute("id");
+		users.remove(user_id);
 		request.getSession().setAttribute("id", null);
 	}
-	
-	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getSession().setAttribute("skip", ((List<String>) request.getServletContext().getAttribute("messages")).size());
-	}
-	
-	protected void checkUsers (HttpServletRequest request, HttpServletResponse response) {
-		Map <Integer,Date> time = (Map<Integer, Date>) request.getServletContext().getAttribute("time");
-		Map <Integer,String> users = (Map<Integer, String>) request.getServletContext().getAttribute("users");
-		
-		Date actualTime = new Date();
-		
-		for (Entry<Integer, Date> itr : time.entrySet()) {
-			if (actualTime.getTime() - itr.getValue().getTime() > 30*1000) {
-				users.remove(itr.getKey());	
-			}
-		}
-	}
-
 }
